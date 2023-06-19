a5037f34ebad4469ad64639ddab5045d41b72ada
cjparsonson <cjparsonson@gmail.com>
2023-06-19 13:33:43 +0100

    Add project files.

---
+++ PacktLibrary/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace Packt.Shared
+{
+    public class Person : Object
+    {
+        // Properties
+        public string? Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        // Methods
+        public void WriteToConsole()
+        {
+            WriteLine($"{Name} was born on a {DateOfBirth:dddd}.");
+        }
+    }
+}
---
+++ PeopleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using Packt.Shared;
+
+Person harry = new()
+{
+    Name = "Harry",
+    DateOfBirth = new(year: 2001, month: 3, day: 25)
+};
+harry.WriteToConsole();
+
+// non-generic lookup collection
+System.Collections.Hashtable lookupObject = new();
+lookupObject.Add(key: 1, value: "Alpha");
+lookupObject.Add(key: 2, value: "Beta");
+lookupObject.Add(key: 3, value: "Gawmma");
+lookupObject.Add(key: harry, value: "Delta");
+
+int key = 2; // lookup a value which has 2 as its key
+WriteLine(format: "key: {0} has value: {1}",
+    arg0: key,
+    arg1: lookupObject[key]);
+
+// Lookup value with harry as its key
+WriteLine(format: "key: {0} has value: {1}",
+    arg0: harry,
+    arg1: lookupObject[harry]);
+
+// generic lookup collection
+Dictionary<int, string> lookupIntString = new();
+lookupIntString.Add(key: 1, value: "Alpha");
+lookupIntString.Add(key: 2, value: "Beta");
+lookupIntString.Add(key: 3, value: "Gawmma");
+lookupIntString.Add(key: harry, value: "Delta");
