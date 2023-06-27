b436c645be54c68a15cdef28f30809e73ff577d8
cjparsonson <cjparsonson@gmail.com>
2023-06-27 13:38:38 +0100

    Start nullable types

---
+++ NullHandling/Program.cs
@@ -0,0 +1,15 @@
+﻿int thisCannotBeNull = 4;
+// thisCannotBeNull = null; // compile error!
+WriteLine(thisCannotBeNull);
+int? thisCouldBeNull = null;
+WriteLine(thisCouldBeNull);
+WriteLine(thisCouldBeNull.GetValueOrDefault());
+thisCouldBeNull = 7;
+WriteLine(thisCouldBeNull);
+WriteLine(thisCouldBeNull.GetValueOrDefault());
+
+
+// The actual type of int? is Nullable<int>
+Nullable<int> thisCouldAlsoBeNull = null;
+thisCouldAlsoBeNull = 7;
+WriteLine(thisCouldAlsoBeNull);
